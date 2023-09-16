7268086a1cd4a0355ddda880b29bb176b12ba703
Abhijeet Jain <abhijeetjain456@gmail.com>
2023-09-16 21:26:10 +0530

    try class component

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,39 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import {Button} from "@mui/material";
+import { Button } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button variant="outlined">Hello World</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    date: new Date(),
+  };
+
+  render() {
+    setInterval(() => {
+      this.setState({
+        date: new Date(),
+      });
+    }, 1000);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <Button variant="outlined">{this.state.date.toLocaleTimeString()}</Button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
